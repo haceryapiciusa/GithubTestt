9d5e84e43ce5b9e2b5c87ab80104c5a1046932aa
haceryapiciusa <56479407+haceryapiciusa@users.noreply.github.com>
2019-11-17 15:54:50 -0500

    neww uptated about branch

--- src/day33/CarBranch.java
+++ src/day33/CarBranch.java
@@ -1,4 +1,13 @@
 package day33;
 
 public class CarBranch {
+    public String model1;
+    public String model2;
+
+    public String getModel1() {
+        return model1;
+    }
+    public String getModel2(){
+        return model2;
+    }
 }
